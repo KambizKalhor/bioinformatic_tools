941d69d80190e2fc7f9bf3181f2c725f1105c04a
Kambiz Kalhor <98652463+KambizKalhor@users.noreply.github.com>
2025-12-19 13:53:33 -0800

    Update and rename new.txt to build_pangaea.sh

---
+++ Pangaea/build_pangaea.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=asteen_1130
+#SBATCH --partition=main
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --time=05:00:00
+
+
+# ==== USAGE ====
+# this script is to build Pangaea
+# sbatch build_pangaea.sh /path/to/apptainer_cache
+# =================
+
+if [ -z "$1" ]; then
+    echo "ERROR: You must provide a path for APPTAINER_CACHEDIR"
+    echo "Usage: $0 /path/to/cache_dir"
+    exit 1
+fi
+
+# 1. Set Apptainer cache directory
+APPTAINER_CACHEDIR="$1"
+export APPTAINER_CACHEDIR
+
+# Make the directory if it doesn't exist
+mkdir -p "$APPTAINER_CACHEDIR"
+
+echo "Using Apptainer cache directory: $APPTAINER_CACHEDIR"
+
+# 2. Load Apptainer
+module load apptainer
+
+# 3. Download the definition file
+curl -O https://raw.githubusercontent.com/KambizKalhor/bioinformatic_tools/master/Pangaea.def
+
+# 4. Build the SIF image
+apptainer build Pangaea.sif Pangaea.def
